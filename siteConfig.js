eccf874f7d20aea40ed2dfe49f27fd2e90b7e04e
data-88 <andrew@thecyberium.com>
2021-01-08 13:20:18 +0200

    artist

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "Andrew Brauteseth",
   shortName: "Brauteseth",
-  description: "Artist  ",
+  description: "Bear with me as we turn the ship around...",
   url: "https://brauteseth.art", // Domain of your site without prefix!
   prefix: "/",
   author: "Imed Adel",
